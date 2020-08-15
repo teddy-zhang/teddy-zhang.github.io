9d02bbb5afa59c53c9dd63a2cb9791e91e728f35
teddy-zhang <53926281+teddy-zhang@users.noreply.github.com>
2020-08-14 20:45:42 -0700

    got rid of unessecary scrollbar, tried (unsuccessfully) to fix navbar slide in animation positioning

--- js/navBar.js
+++ js/navBar.js
@@ -4,15 +4,19 @@ var onMenuIconHover = function(){
   menuIconEl.style.visibility = "hidden";
   navBarEl.style.visibility = "visible";
   var startTime = new Date().getTime();
+
   var moveInFromRight = function() {
     console.log("goingonce");
     var currTime = new Date().getTime();
-    var newLeft = (100 - ((currTime - startTime)/1000) * 300);
+    var newLeft = (100 - ((currTime - startTime)/1000) * 100);
     navBarEl.style.left = newLeft + "%";
-    if (newLeft > 0.01) {
+    if (newLeft > 0.745) {
       window.requestAnimationFrame(moveInFromRight);
+    }
+    else {
+      window.cancelAnimationFrame(moveInFromRight);
     }
   };
   moveInFromRight();
 };
-menuIconEl.addEventListener("mouseenter", onMenuIconHover);
+menuIconEl.addEventListener("mousedown", onMenuIconHover);
