c83681de5d516281d45a4c37b1a02ceb30cdba28
teddy-zhang <53926281+teddy-zhang@users.noreply.github.com>
2020-08-16 03:17:44 -0700

    added video caption/description that appears on hover

--- js/youtube.js
+++ js/youtube.js
@@ -1,11 +1,14 @@
 var ompWeber = document.getElementById("ompWeber");
 var ompWeberVid = document.getElementById("ompWeberVid");
+var caption = document.getElementById("ompWeberCaption");
 var onOMPHover = function(){
   console.log("hover detected");
   //var startTime = new Date().getTime();
   ompWeber.style.backgroundColor = "#89b8d5";
   ompWeberVid.style.width = "560px";
   ompWeberVid.style.height = "315px";
+  ompWeberCaption.style.visibility = "visible";
+  // ompWeber.insertAdjacentHTML("afterend", "<p>hi</p>");
   //UNECESSARY BC OF CSS TRANSITION
   // var enlarge = function() {
   //   console.log("enlarging");
@@ -31,6 +34,7 @@ var onOMPHoverOff = function(){
   ompWeber.style.backgroundColor = "#C1DAE9";
   ompWeberVid.style.width = "200px";
   ompWeberVid.style.height = "112.5px";
+  ompWeberCaption.style.visibility = "hidden";
 };
 ompWeber.addEventListener("mouseover", onOMPHover);
 ompWeber.addEventListener("mouseout", onOMPHoverOff);
@@ -39,12 +43,14 @@ ompWeber.addEventListener("mouseout", onOMPHoverOff);
 
 var bozza = document.getElementById("bozza");
 var bozzaVid = document.getElementById("bozzaVid");
+var caption = document.getElementById("bozzaCaption");
 var onBozzaHover = function(){
   console.log("hover detected");
   //var startTime = new Date().getTime();
   bozza.style.backgroundColor = "#89b8d5";
   bozzaVid.style.width = "560px";
   bozzaVid.style.height = "315px";
+  bozzaCaption.style.visibility = "visible";
   //UNECESSARY BC OF CSS TRANSITION
   // var enlarge = function() {
   //   var currTime = new Date().getTime();
@@ -68,6 +74,7 @@ var onBozzaHoverOff = function(){
   bozza.style.backgroundColor = "#C1DAE9";
   bozzaVid.style.width = "200px";
   bozzaVid.style.height = "112.5px";
+  bozzaCaption.style.visibility = "hidden";
 };
-bozza.addEventListener("mouseenter", onBozzaHover);
+bozza.addEventListener("mouseover", onBozzaHover);
 bozza.addEventListener("mouseout", onBozzaHoverOff);
