dfc061ae0de69dc67a6631865c8ad5d18e256151
teddy-zhang <53926281+teddy-zhang@users.noreply.github.com>
2020-08-16 03:53:18 -0700

    added menu icon into navbar for exit, plan to add code for clicking that <li> that makes the nav bar retreat and plan to fix the menu icon to white in the navbar and rearrange the two to look better

--- js/navBar.js
+++ js/navBar.js
@@ -1,22 +1,25 @@
 var menuIconEl = document.getElementById("menuicon");
 var navBarEl = document.getElementById("navBar");
+var videos = document.getElementById("ompWeber");
 var onMenuIconHover = function(){
   menuIconEl.style.visibility = "hidden";
   navBarEl.style.visibility = "visible";
-  var startTime = new Date().getTime();
+  navBarEl.style.left = "0%";
+  ompWeber.style.marginTop = "5%";
+  // var startTime = new Date().getTime();
 
-  var moveInFromRight = function() {
-    console.log("goingonce");
-    var currTime = new Date().getTime();
-    var newLeft = (100 - ((currTime - startTime)/1000) * 100);
-    navBarEl.style.left = newLeft + "%";
-    if (newLeft > 0.745) {
-      window.requestAnimationFrame(moveInFromRight);
-    }
-    else {
-      window.cancelAnimationFrame(moveInFromRight);
-    }
-  };
-  moveInFromRight();
+  // var moveInFromRight = function() {
+  //   var currTime = new Date().getTime();
+  //   var newLeft = (100 - ((currTime - startTime)/1000) * 100);
+  //   navBarEl.style.left = newLeft + "%";
+  //   if (newLeft > 0.745) {
+  //     window.requestAnimationFrame(moveInFromRight);
+  //   }
+  //   else {
+  //     window.cancelAnimationFrame(moveInFromRight);
+  //   }
+  // };
+  // moveInFromRight();
 };
 menuIconEl.addEventListener("mousedown", onMenuIconHover);
+//add function for unhover
